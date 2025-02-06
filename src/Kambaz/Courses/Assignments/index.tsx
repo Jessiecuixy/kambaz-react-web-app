3bd7dcad3e28c9269a22201123ec1b6c233d8944
Xueyang Cui <cui.xue@northeastern.edu>
2025-02-05 22:49:11 -0500

    Modify Assignments

--- src/Kambaz/Courses/Assignments/index.tsx
+++ src/Kambaz/Courses/Assignments/index.tsx
@@ -1,8 +1,9 @@
-import "../../styles.css";
 import { Button, ListGroup } from "react-bootstrap";
-import { BsGripVertical, BsThreeDotsVertical } from "react-icons/bs";
+import { BsGripVertical } from "react-icons/bs";
 import { FaCheckCircle, FaPlus } from "react-icons/fa";
 import { LiaFileAltSolid, LiaPlusSolid, LiaSearchSolid } from "react-icons/lia";
+import { GoTriangleDown } from "react-icons/go";
+import { IoEllipsisVertical } from "react-icons/io5";
 
 export default function Assignments() {
   return (
@@ -20,7 +21,7 @@ export default function Assignments() {
           />
         </div>
         <div>
-          <Button variant="secondary" size="lg" className="me-2" id="wd-add-assignment-group">
+          <Button variant="light" size="lg" className="me-2" id="wd-add-assignment-group">
             <LiaPlusSolid className="me-1" /> Group
           </Button>
           <Button variant="danger" size="lg" id="wd-add-assignment">
@@ -29,55 +30,55 @@ export default function Assignments() {
         </div>
       </div>
 
-      <ListGroup id="wd-assignments">
-        <ListGroup.Item className="d-flex align-items-center p-3 bg-light fw-bold border">
+      <ListGroup id="wd-assignments" className="wd-lessons rounded-0">
+        <div className="d-flex align-items-center p-3 bg-light fw-bold border wd-title ps-2">
           <BsGripVertical className="me-2 text-secondary fs-4" />
-          <span className="fs-5">ASSIGNMENTS</span>
-          <span className="ms-auto text-muted fs-6 border rounded px-2 py-1">40% of Total</span>
+          <span className="fs-5">  <GoTriangleDown /> ASSIGNMENTS</span>
+          <span className="ms-auto text-muted fs-6 border rounded-pill px-2 py-1">40% of Total</span>
           <FaPlus className="ms-3 text-secondary" />
-          <BsThreeDotsVertical className="text-secondary fs-5 ms-3 end-0 me-3" />
-        </ListGroup.Item>
+          <IoEllipsisVertical className="text-secondary fs-4 ms-2" />
+        </div>
 
-        <ListGroup.Item className="d-flex align-items-center p-3 border-start-green">
-          <BsGripVertical className="me-3 text-secondary fs-5" />
-          <LiaFileAltSolid className="me-3 text-success fs-4" />
+        <ListGroup.Item className="d-flex align-items-center wd-lesson p-3 ps-1">
+          <BsGripVertical className="me-3 text-secondary fs-4" />
+          <LiaFileAltSolid className="me-4 text-success fs-4" />
           <div className="flex-grow-1">
-            <a href="#/Kambaz/Courses/1234/Assignments/123" className="fw-bold text-dark fs-5">A1</a>
+            <a href="#/Kambaz/Courses/1234/Assignments/123" className="fw-bold text-dark text-decoration-none fs-5">A1</a>
             <div className="text-muted small">
               <span className="text-danger fw-bold">Multiple Modules</span> | <b>Not available until</b> May 6 at 12:00am |<br />
               <b>Due</b> May 13 at 11:59pm | 100 pts
             </div>
           </div>
           <FaCheckCircle className="text-success fs-4" />
-          <BsThreeDotsVertical className="text-secondary fs-5 ms-3 end-0 me-3" />
+          <IoEllipsisVertical className="text-secondary fs-4 ms-2" />
         </ListGroup.Item>
 
-        <ListGroup.Item className="d-flex align-items-center p-3 border-start-green">
-          <BsGripVertical className="me-3 text-secondary fs-5" />
-          <LiaFileAltSolid className="me-3 text-success fs-4" />
+        <ListGroup.Item className="d-flex align-items-center wd-lesson p-3 ps-1">
+          <BsGripVertical className="me-3 text-secondary fs-4" />
+          <LiaFileAltSolid className="me-4 text-success fs-4" />
           <div className="flex-grow-1">
-            <a href="#/Kambaz/Courses/1234/Assignments/123" className="fw-bold text-dark fs-5">A2</a>
+            <a href="#/Kambaz/Courses/1234/Assignments/123" className="fw-bold text-dark text-decoration-none fs-5">A2</a>
             <div className="text-muted small">
               <span className="text-danger fw-bold">Multiple Modules</span> | <b>Not available until</b> May 13 at 12:00am |<br />
               <b>Due</b> May 20 at 11:59pm | 100 pts
             </div>
           </div>
           <FaCheckCircle className="text-success fs-4" />
-          <BsThreeDotsVertical className="text-secondary fs-5 ms-3 end-0 me-3" />
+          <IoEllipsisVertical className="text-secondary fs-4 ms-2" />
         </ListGroup.Item>
 
-        <ListGroup.Item className="d-flex align-items-center p-3 border-start-green">
-          <BsGripVertical className="me-3 text-secondary fs-5" />
-          <LiaFileAltSolid className="me-3 text-success fs-4" />
+        <ListGroup.Item className="d-flex align-items-center wd-lesson p-3 ps-1">
+          <BsGripVertical className="me-3 text-secondary fs-4" />
+          <LiaFileAltSolid className="me-4 text-success fs-4" />
           <div className="flex-grow-1">
-            <a href="#/Kambaz/Courses/1234/Assignments/123" className="fw-bold text-dark fs-5">A3</a>
+            <a href="#/Kambaz/Courses/1234/Assignments/123" className="fw-bold text-dark text-decoration-none fs-5">A3</a>
             <div className="text-muted small">
               <span className="text-danger fw-bold">Multiple Modules</span> | <b>Not available until</b> May 20 at 12:00am |<br />
               <b>Due</b> May 27 at 11:59pm | 100 pts
             </div>
           </div>
           <FaCheckCircle className="text-success fs-4" />
-          <BsThreeDotsVertical className="text-secondary fs-5 ms-3 end-0 me-3" />
+          <IoEllipsisVertical className="text-secondary fs-4 ms-2" />
         </ListGroup.Item>
       </ListGroup>
     </div>
