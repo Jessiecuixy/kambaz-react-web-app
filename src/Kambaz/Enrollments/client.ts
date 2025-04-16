412de6bb382cb202961ee252f17e53fe1ba0c4d9
Xueyang Cui <cui.xue@northeastern.edu>
2025-04-16 11:42:00 -0400

    fix error

--- src/Kambaz/Enrollments/client.ts
+++ src/Kambaz/Enrollments/client.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 const REMOTE_SERVER = import.meta.env.VITE_REMOTE_SERVER;
-const ENROLLMENTS_API = `${REMOTE_SERVER}/api/enrollments`;
+// const ENROLLMENTS_API = `${REMOTE_SERVER}/api/enrollments`;
 
 export const enrollCourse = async (enrollment: any) => {
   const response = await axios.post(`${REMOTE_SERVER}/api/users/${enrollment.user}/courses/${enrollment.course}`);
